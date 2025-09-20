29dc2a7f95f0b1afc9f10962d41e940d46a7bb1f
Gowtham <gowtham@coderabbit.ai>
2025-09-19 18:38:58 -0700

    ADding new code

--- javas/trial.Java
+++ javas/trial.Java
@@ -1,6 +1,9 @@
 package com.example;
 
 import com.google.gson.Gson;
+import org.apache.commons.lang3.StringUtils;   // Apache Commons Lang
+import com.google.common.collect.ImmutableMap; // Google Guava
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,5 +24,18 @@ public class App {
         @SuppressWarnings("unchecked")
         Map<String, Object> parsed = gson.fromJson(json, Map.class);
         System.out.println("Parsed back: " + parsed);
+
+        // Using Apache Commons Lang
+        String name = (String) parsed.get("name");
+        if (StringUtils.isNotBlank(name)) {
+            System.out.println("Name is not blank: " + name.toUpperCase());
+        }
+
+        // Using Guava ImmutableMap
+        Map<String, String> country = ImmutableMap.of(
+                "IN", "India",
+                "US", "United States"
+        );
+        System.out.println("Countries: " + country);
     }
 }
