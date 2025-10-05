1014ede86edeaa9d067624821f9cdd29bca5f2d2
Gowtham <gowtham@coderabbit.ai>
2025-10-05 14:53:34 -0700

    Adding changes

--- fools/files.ts
+++ fools/files.ts
@@ -24,7 +24,7 @@ export const UserSchema = z.object({
     bio: z.string().optional(),
     joined: z.date(),
   }),
-  address: z.url({ message: "Invalid address URL" }).optional()
+  address: z.urlsssss({ message: "Invalid address URL" }).optional()
 })
 
 // ✅ TypeScript inference
