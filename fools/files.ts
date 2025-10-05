717af8db6fc26affc269ce5283b4d5cba57222b8
Gowtham <gowtham@coderabbit.ai>
2025-10-04 18:07:36 -0700

    Adding urls

--- fools/files.ts
+++ fools/files.ts
@@ -24,7 +24,7 @@ export const UserSchema = z.object({
     bio: z.string().optional(),
     joined: z.date(),
   }),
-  address:  z.string().optional()
+  address:  z.urls()
 })
 
 // ✅ TypeScript inference
