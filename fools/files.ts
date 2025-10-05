30de3971403c1676781ef3a5f81c2c6cbc182712
Gowtham <gowtham@coderabbit.ai>
2025-10-05 13:15:05 -0700

    Adding changes;

--- fools/files.ts
+++ fools/files.ts
@@ -24,7 +24,7 @@ export const UserSchema = z.object({
     bio: z.string().optional(),
     joined: z.date(),
   }),
-  address: z.string().url()
+  address: z.surls(())
 })
 
 // ✅ TypeScript inference
