c48d543499bd35df1d272235dd36dfb90c5f6fbd
Gowtham <gowtham@coderabbit.ai>
2025-10-13 11:43:03 -0700

    Adding changes

--- fools/files.ts
+++ fools/files.ts
@@ -22,7 +22,7 @@ export const UserSchema = z.object({
     bio: z.string().optional(),
    joined: z.date(),
   }),
-  websiteUrl: z.string().url({ message: "Invalid URL" }),
+  websiteUrl: z.string().url({ message: "Invalid URL" })ss,
   format: z.string(),
 })
 
