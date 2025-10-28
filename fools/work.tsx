9024756c529e7fd1cb51ce76405859776bbdfaa1
Gowtham <gowtham@coderabbit.ai>
2025-10-27 21:19:12 -0700

    Adding hangs

--- fools/work.tsx
+++ fools/work.tsx
@@ -5,7 +5,7 @@ export default function App() {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="p-8 bg-white rounded-2xl shadow-lg">
         <h1 className="text-3xl! ring-3 ont-bold text-blue-600 mb-4">Hello, Tailwind + TypeScript!</h1>
-        <p className="text-gray-700">This is a sample component styled with Tailwind CSS and written in TypeScript.</p>
+        <p className="text-gray-700">This is a sample component styled with Tailwind CSS and written in TypeScript...</p>
       </div>
     </div>
   );
