a6824bc5e80d1fff39619f3ac73ffa156d0eaef3
Gowtham <gowtham@coderabbit.ai>
2025-11-05 11:56:42 -0800

    Adding changes

--- fools/file.ts
+++ fools/file.ts
@@ -32,3 +32,17 @@ export function parseUser(input: unknown) {
   }
   return result.data
 }
+
+
+
+
+import { createAuthClient } from "better-auth/react";
+import { env } from "@/env";
+import { ssoClient } from "@better-auth/sso/client";
+import { organizationClient } from "better-auth/client/plugins";
+
+export const { signIn, signOut, signUp, useSession, getSession, sso } =
+  createAuthClient({
+    baseURL: env.NEXT_PUBLIC_BASE_URL,
+    plugins: [ssoClient(), organizationClient()],
+  });
