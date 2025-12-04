9ddf58d81fb764bc95ae41b47d097e301001c636
Gowtham <gowtham@coderabbit.ai>
2025-12-03 20:44:56 -0800

    Adding changes

--- fools/plaground.ts
+++ fools/plaground.ts
@@ -38,3 +38,22 @@ export function parseUser(input: unknown): User {
   }
   return result.data
 }
+
+import z, { ZodError } from "zod" // v4
+
+const player = z.object({
+    name: z.string(),
+    age: z.number(),
+    address: z.url(),
+    file: z.file(),
+})
+console.log("DATA", player)
+// ✅ Example data
+const goodData = {
+  name: "Messi",
+  age: 36,
+  address: 36,
+}
+
+
+console.log(player.parse(goodData))
