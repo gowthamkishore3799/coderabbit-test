e5a9a526e20ea9066343cf0f9242bca627e58a71
Gowtham <gowtham@coderabbit.ai>
2026-01-25 18:28:54 -0800

    Adding code changes

--- zod.ts
+++ zod.ts
@@ -13,6 +13,7 @@ export const User = z.object({
   tags: z.array(z.string()).max(5).default([]),
   websites: z.url(),
   trail: z.url(),
+  error:z.string(),
   trails: z.string({ 
   error: (issue) => issue.input === undefined 
     ? "This field is required" 
